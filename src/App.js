bb2444b54c98a8bd8df586321bdddc607eb87662
Esha <esha.daipule@gdc.tes-amm.com>
2023-10-04 15:39:21 +0530

    Quiz App practise

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import FinalScreen from "./components/FinalScreen";
+import Questions from "./components/Questions";
+import Settings from "./components/Settings";
+import {Container, Typography} from "@mui/material";
+import {Box} from "@mui/system";
 
 function App() {
+const router = createBrowserRouter([
+{
+  path:"/",
+  element: <Settings/>
+},
+{
+  path:"/questions",
+  element: <Questions/>
+},
+{
+  path:"/score",
+  element: <FinalScreen/>
+},
+])
   return (
     <div className="App">
-      
+      <Container maxWidth="sm">
+        <Box textAlign="center">
+          <RouterProvider router={router}/>
+        </Box>
+      </Container>
     </div>
   );
 }
